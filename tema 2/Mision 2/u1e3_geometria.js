cd63b7056e27c4001b6c4b0ad75a257ac1902fc3
unknown <oscarmaller43@gmail.com>
2020-10-19 05:07:21 +0200

    3

---
+++ tema 2/Mision 2/u1e3_geometria.js
@@ -0,0 +1,36 @@
+//En este fichero estan las formulas.
+function areaTriangulo (b,a){
+    return b * a / 2;
+}
+
+function permietroTrianguloEquilatero(l){
+    return l + l + l;
+}
+
+function perimetroTrianguloIsosceles(l1, l2, b){
+    return l1 + l2 + b;
+}
+
+function permietroTrianguloEscaleno(l1,l2,l3){
+    return l1 + l2 + l3;
+}
+
+function areaCuadrado(l){
+    return l += 2;
+}
+
+function perimetroCuadrado(l){
+    return l + l + l + l;
+}
+
+function areaRectangulo(l1,l2){
+    return l1 * l2;
+}
+
+function areaCirculo(r){
+    return r *= 2;
+}
+
+function longitudCircunferencia(r){
+    return 2 * 3.14 * r;
+}
